236c8f7355b3f972c44144bcd52d427ea2d79014
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2025-07-18 22:24:09 -0400

    working on basic measurements

--- Diagnostics/Assets/Basic/BasicMeasurementConfiguration.cs
+++ Diagnostics/Assets/Basic/BasicMeasurementConfiguration.cs
@@ -5,6 +5,8 @@ using System.Xml.Serialization;
 [XmlInclude(typeof(LDL.LDLMeasurementSettings))]
 public class BasicMeasurementConfiguration
 {
+    [Category("Config")]
+    [Description("This sets the filename")]
     public string Name { get; set; }
     private bool ShouldSerializeName() {  return false; }
 }
